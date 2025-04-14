6cc08fdd32c2c45df45c2baec7b41ae644a75f17
JoshuaTuheirwe <tuheirwejoshua@gmail.com>
2025-04-14 11:17:21 +0300

    Update footer.php
    changes made according to chapter 6

--- footer.php
+++ footer.php
@@ -1,15 +1,24 @@
-<footer class="bg-dark text-white py-2">
-    <div class="container d-flex align-items-center justify-content-between">
-        <p class="fw-bold m-0">READY FOR <br> <span>THE <span class="text-danger">NEW</span></span> EXPERIENCE</p>
-        <div class="contact">
-            <p>Contact us on:</p>
-            <p>+25689343457 | +25678794065</p>
-            <p>group6member@gmail.com</p>
-        </div>
-        <div class="social-icons d-flex">
-            <i class="fa-brands fa-instagram me-2"></i>
-            <i class="fa-brands fa-facebook me-2"></i>
-            <i class="fa-brands fa-twitter"></i>
+<footer class="bg-dark text-white py-4">
+    <div class="container">
+        <div class="row text-center text-md-start align-items-center">
+            <!-- Message Section -->
+            <div class="col-md-4 mb-3 mb-md-0">
+                <p class="fw-bold mb-1">READY FOR <br><span>THE <span class="text-danger">NEW</span></span> EXPERIENCE</p>
+            </div>
+
+            <!-- Contact Info -->
+            <div class="col-md-4 mb-3 mb-md-0">
+                <p class="mb-1">Contact us on:</p>
+                <p class="mb-1">+25689343457 | +25678794065</p>
+                <p class="mb-0">group6member@gmail.com</p>
+            </div>
+
+            <!-- Social Icons -->
+            <div class="col-md-4 d-flex justify-content-center justify-content-md-end">
+                <a href="#" class="text-white me-3"><i class="fab fa-instagram fa-lg"></i></a>
+                <a href="#" class="text-white me-3"><i class="fab fa-facebook fa-lg"></i></a>
+                <a href="#" class="text-white"><i class="fab fa-twitter fa-lg"></i></a>
+            </div>
         </div>
     </div>
 </footer>
