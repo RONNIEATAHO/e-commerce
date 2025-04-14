f66a84e74fc577c82c5f06b534f32abf278e7a32
JoshuaTuheirwe <tuheirwejoshua@gmail.com>
2025-04-14 10:49:11 +0300

    Update account.php
    updated basing on lecture 6

--- account.php
+++ account.php
@@ -4,6 +4,17 @@ if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit;
 }
+
+// ========== STRUCTURAL MODULARIZATION ==========
+
+function getSanitizedSessionValue($key) {
+    return isset($_SESSION[$key]) ? htmlspecialchars($_SESSION[$key]) : '';
+}
+
+function getProfilePicturePath() {
+    $file = getSanitizedSessionValue('profile_picture');
+    return "uploads/" . $file;
+}
 ?>
 
 <!DOCTYPE html>
@@ -16,8 +27,8 @@ if (!isset($_SESSION['user_id'])) {
     <link rel="stylesheet" href="account.css">
 </head>
 <body>
-    <h1>Welcome, <?= htmlspecialchars($_SESSION['username']); ?></h1>
-    <img src="uploads/<?= htmlspecialchars($_SESSION['profile_picture']); ?>" alt="Profile Picture" width="150" height="150">
-    <p>Profile Picture: <?= htmlspecialchars($_SESSION['profile_picture']); ?></p>
+    <h1>Welcome, <?= getSanitizedSessionValue('username'); ?></h1>
+    <img src="<?= getProfilePicturePath(); ?>" alt="Profile Picture" width="150" height="150">
+    <p>Profile Picture: <?= getSanitizedSessionValue('profile_picture'); ?></p>
 </body>
 </html>
