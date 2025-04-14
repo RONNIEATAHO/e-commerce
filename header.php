d9cb689255149ad10b9895fb0c89727ad621f8b5
JoshuaTuheirwe <tuheirwejoshua@gmail.com>
2025-04-14 11:12:28 +0300

    Update header.php
    changes made according to chapter 6

--- header.php
+++ header.php
@@ -1,9 +1,11 @@
-    <header class="bg-light p-3">
-        <div class="container d-flex justify-content-between align-items-center">
-            <h1 class="fw-bold">LEARNING <br> <span class="text-primary">MADE</span> EASY</h1>
-            <nav>
-                <a href="#" class="me-3 text-dark text-decoration-none">ABOUT US</a>
-                <a href="#" class="btn btn-primary">HOME</a>
-            </nav>
-        </div>
-    </header> 
+<header class="bg-light py-3">
+    <div class="container d-flex flex-column flex-md-row justify-content-between align-items-center">
+        <h1 class="fw-bold mb-2 mb-md-0">
+            LEARNING <br><span class="text-primary">MADE</span> EASY
+        </h1>
+        <nav>
+            <a href="#" class="me-3 text-dark text-decoration-none">ABOUT US</a>
+            <a href="#" class="btn btn-primary">HOME</a>
+        </nav>
+    </div>
+</header>
